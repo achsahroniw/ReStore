34143f776f76affd4ff5d16658282bd2afd15c2a
achsahroniw <achsahroniw@gmail.com>
2024-03-25 13:18:38 +0700

    Re-commit (end of section 6)

---
+++ grocery_store/src/app/store/configureStore.ts
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../../features/contact/counterReducer";
+
+export function configureStore() {
+  return configureStore();
+}
